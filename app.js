27120acc5d31b5e86cb12e8cf64586d5afb05d07
sohailriyas <riyas.sohail@gmail.com>
2022-11-15 17:49:07 +0530

    changes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require ('express')
 const app = express()
 
-app.get ('/api', (req,res) => res.send('!!!Hello World'))
+app.get ('/api', (req,res) => res.send('Hello World'))
 app.listen (3000, () => console.log('Second server ready'))
